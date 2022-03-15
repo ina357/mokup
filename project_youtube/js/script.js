93a6dca06fb9d7f9ec64292e841b54c59ac9e422
ina357 <nature10017@naver.com>
2022-03-15 12:57:51 +0900

    first commit

---
+++ project_youtube/js/script.js
@@ -0,0 +1,6 @@
+const moreBtn=document.querySelector('.info .moreBtn');
+const title=document.querySelector('.info .metadata .title');
+moreBtn.addEventListener('click',()=>{
+    title.classList.toggle('addClick');
+    moreBtn.classList.toggle('clicked');
+});
